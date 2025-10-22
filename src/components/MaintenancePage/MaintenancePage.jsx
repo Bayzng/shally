93cf9fae03807c1f1ccd7f475505a669c62d4def
almustanir <olajidemubarak561@gmail.com>
2025-10-22 12:10:13 +0100

    dev mode

--- src/components/MaintenancePage/MaintenancePage.jsx
+++ src/components/MaintenancePage/MaintenancePage.jsx
@@ -58,7 +58,7 @@ const MaintenancePage = () => {
 
       {/* Featured Section */}
       <section className="container mx-auto px-6 py-20">
-        <h2 className="text-4xl font-bold text-center mb-12 text-white">
+        <h2 className="text-4xl font-bold text-center mb-12">
           Preview of Featured Products
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
