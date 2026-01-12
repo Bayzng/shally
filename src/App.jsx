02d042289b88388b1c8e020f99f0e4c9ebc2ba6e
Bayz.ng <108221204+Bayzng@users.noreply.github.com>
2026-01-12 14:36:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import Allproducts from './pages/allproducts/Allproducts';
 import TransactionStatus from "./components/TransactionStatus/TransactionStatus";
 import OrderHistory from "./components/OrderHistory/OrderHistory";
-import MaintenancePage from "./components/MaintenancePage/MaintenancePage";
-// import Home from './pages/home/Home';
+// import MaintenancePage from "./components/MaintenancePage/MaintenancePage";
+import Home from './pages/home/Home';
 
 
 function App() {
@@ -29,8 +29,8 @@ function App() {
     <MyState>
       <Router>
         <Routes>
-          <Route path="/" element={<MaintenancePage />} /> 
-         {/*<Route path="/" element={<Home />} /> */}
+          {/* <Route path="/" element={<MaintenancePage />} /> */}
+         <Route path="/" element={<Home />} /> 
           <Route path="/allproducts" element={<Allproducts />} />
           <Route path="/order" element={
             <ProtectedRoute>
